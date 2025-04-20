358f70361cefcd3a68e68bb4c1d7a37b577df508
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-04-21 00:22:01 +0400

    init

---
+++ src/main/java/ru/derendyaev/ideathesis_users_etl/IdeathesisUsersEtlApplication.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.ideathesis_users_etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeathesisUsersEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeathesisUsersEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/derendyaev/ideathesis_users_etl/IdeathesisUsersEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.ideathesis_users_etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeathesisUsersEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
